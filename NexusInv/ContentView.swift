baed12c2dcd347f725d394dfaa7b5040fa3b4914
Elias Nuwara <eliasab@college.harvard.edu>
2022-07-15 06:05:59 -0400

    Initial Commit

---
+++ NexusInv/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NexusInv
+//
+//  Created by Elias Abunuwara on 2022-07-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NexusInv/NexusInvApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusInvApp.swift
+//  NexusInv
+//
+//  Created by Elias Abunuwara on 2022-07-15.
+//
+
+import SwiftUI
+
+@main
+struct NexusInvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
